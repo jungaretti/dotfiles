8cd2d3ee9cef15859da73b95221e71d3c137d878
JP Ungaretti <hello@jungaretti.com>
2021-10-13 00:09:43 -0700

    Categorize winget packages

--- stuff/windows/install.ps1
+++ stuff/windows/install.ps1
@@ -1,11 +1,15 @@
 winget install --id "AgileBits.1Password"
 winget install --id "Discord.Discord"
-winget install --id "Git.Git"
 winget install --id "Mozilla.Firefox"
+
+# Gaming
 winget install --id "Valve.Steam"
 winget install --id "Ubisoft.Connect"
-winget install --id "vim.vim"
-winget install --id "Microsoft.PowerShell"
-winget install --id "Microsoft.VisualStudioCode"
-winget install --id "GitHub.cli"
 winget install --id "EpicGames.EpicGamesLauncher"
+
+# Devtools
+winget install --id "vim.vim"
+winget install --id "Git.Git"
+winget install --id "GitHub.cli"
+winget install --id "Microsoft.VisualStudioCode"
+winget install --id "Microsoft.PowerShell"

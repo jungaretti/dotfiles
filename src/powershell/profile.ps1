328bcabcbfde823848533794c43d0dc19c169fe2
JP Ungaretti <hello@jungaretti.com>
2022-01-25 11:14:13 -0800

    Fix pwsh theme

--- src/powershell/profile.ps1
+++ src/powershell/profile.ps1
@@ -1,4 +1,4 @@
 Import-Module git-aliases -DisableNameChecking
 Import-Module oh-my-posh
 Import-Module posh-git
-Set-PoshPrompt -Theme "$($HOME)\Documents\PowerShell\wezmps.omp.json"
+Set-PoshPrompt -Theme "zash"

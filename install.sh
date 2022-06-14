e7a419cfdd1babae92e73d2147ba50c7452b7ad9
JP Ungaretti <hello@jungaretti.com>
2022-06-14 14:33:10 -0700

    Port essential links to new script

--- install.sh
+++ install.sh
@@ -7,7 +7,30 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 source "./scripts/dots.sh"
 
-link ~/.vimrc -s src/vim/vimrc
-link ~/.zshrc -s src/zsh/zshrc
-link ~/.config/alacritty/alacritty.yml -s src/alacritty/alacritty.yml -c
-link ~/.gnupg/gpg-agent.conf -s src/gnupg/gpg-agent.conf --if 'gpg -k'
+set +e
+
+link ~/.gitconfig \
+    --src src/git/gitconfig
+link ~/.vimrc \
+    --src src/vim/vimrc
+link ~/.zshrc \
+    --src src/zsh/zshrc \
+    --force
+link ~/.config/alacritty/alacritty.yml \
+    --src src/alacritty/alacritty.yml \
+    --create
+link ~/.config/crestic/crestic.cfg \
+    --src src/crestic/crestic.cfg \
+    --create
+link ~/.config/gh/config.yml \
+    --src src/gh/config.yml \
+    --create
+link ~/.config/restic/exclude.txt \
+    --src src/restic/exclude.txt \
+    --create
+link ~/.config/topgrade.toml \
+    --src src/topgrade/topgrade.toml \
+    --create
+link ~/.gnupg/gpg-agent.conf \
+    --if 'gpg -k' \
+    --src src/gnupg/gpg-agent.conf

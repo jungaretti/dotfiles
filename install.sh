c8cb94dec273649007af2ded098a5b7a70d4d1d8
JP Ungaretti <git@jungaretti.com>
2023-03-13 19:54:01 -0700

    Remove unused Linux dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
@@ -7,9 +7,10 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 source "./scripts/dots.sh"
 
+echo "Instaling dotfiles..."
 start
 
-echo "Instaling dotfiles..."
+# Everywhere
 collect link ~/.gitconfig \
     --src src/git/gitconfig
 collect link ~/.vimrc \
@@ -41,33 +42,5 @@ collect link ~/Library/Application\ Support/iTerm2/DynamicProfiles/Profiles.json
     --if '[ "$(uname -s)" = Darwin ]' \
     --src src/iterm/Profiles.json \
     --create
-
-# Arch Linux with i3 window manager
-collect link ~/.config/dunst/dunstrc \
-    --if '[ "$USE_CUSTOM_WM" = "true" ]' \
-    --src src/dunst/dunstrc \
-    --create
-collect link ~/.config/i3 \
-    --if '[ "$USE_CUSTOM_WM" = "true" ]' \
-    --src src/i3 \
-    --create
-collect link ~/.config/i3status-rust \
-    --if '[ "$USE_CUSTOM_WM" = "true" ]' \
-    --src src/i3status-rust \
-    --create
-collect link ~/.config/rofi \
-    --if '[ "$USE_CUSTOM_WM" = "true" ]' \
-    --src src/rofi \
-    --create
-
-if [ -n "$CODESPACE_NAME" ] && devcontainer-info | grep 'Definition ID: codespaces-linux' &>/dev/null; then
-    echo "Installing kitchensink tools..."
-    
-    echo "Installing ripgrep:"
-    collect sudo apt-get -yq install ripgrep
-
-    echo "Installing fnm:"
-    curl -fsSL https://fnm.vercel.app/install | collect bash
-fi
 
 report

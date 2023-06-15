ea50ad2a483d85609f7e6b60e66fd27261f3a642
JP Ungaretti <git@jungaretti.com>
2023-06-15 15:05:16 +0200

    Add recurse flag

--- clean.sh
+++ clean.sh
@@ -3,12 +3,42 @@
 set -e
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DEBUG="true"
 
 clean() {
-    local dir="$1"
-    
-    fd --type l --search-path "$dir" | while read symlink; do
+    local recurse="false"
+
+    local args=()
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -r | --recurse)
+                recurse="true"
+                shift
+                ;;
+            -* | --*)
+                echo "Unknown option: $1"
+                return 1
+                ;;
+            *)
+                args+=("$1")
+                shift
+                ;;
+        esac
+    done
+
+    # Make sure directory exists
+    local dir="${args[0]}"
+    if [ ! -d "$dir" ]; then
+        echo "Directory does not exist: $dir"
+        return 1
+    fi
+
+    # Use --max-depth to avoid excessive recursion
+    local fd_args=()
+    if [ "$recurse" = "false" ]; then
+        fd_args+=("--max-depth" "1")
+    fi
+
+    fd "${fd_args[@]}" --hidden --type l --search-path "$dir" | while read symlink; do
         local target="$(readlink "$symlink")"
 
         # Only clean symlinks that point to these dotfiles
@@ -27,5 +57,5 @@ clean() {
 }
 
 echo "Cleaning dotfile symlinks..."
-clean "$HOME/.config"
-# clean "$HOME"
+clean --recurse "$HOME/.config"
+clean "$HOME"

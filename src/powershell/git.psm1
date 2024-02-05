6ecdb2d662db9d4c5fa7320702bdf5157f12288f
JP Ungaretti <jpungaretti@microsoft.com>
2024-02-05 13:56:49 -0500

    powershell: Clean up git aliases

--- src/powershell/git.psm1
+++ src/powershell/git.psm1
@@ -1,23 +1,21 @@
-function Git-Status { git status }
-
-function Git-Log { git log --stat }
-
-function Git-Push {
-    $branch = "$(git branch --show-current)"
-    git push origin $branch
+function gst {
+    git status
 }
 
-function Git-Pull {
+function glg {
+    git log --stat
+}
+
+function ggl {
     $branch = "$(git branch --show-current)"
     git pull origin $branch
 }
 
-function Git-AddPick {
-    git add -p .
+function ggp {
+    $branch = "$(git branch --show-current)"
+    git push origin $branch
 }
 
-Set-Alias -Name glg -Value Git-Log
-Set-Alias -Name gst -Value Git-Status
-Set-Alias -Name ggp -Value Git-Push
-Set-Alias -Name ggl -Value Git-Pull
-Set-Alias -Name gap -Value Git-AddPick
+function ggpnp {
+    ggl && ggp
+}
